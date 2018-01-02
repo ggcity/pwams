ad391c86506534e581c47209754fa3d7e48f802e
Rachot Moragraan <janechii@gmail.com>
2018-01-01 23:41:59 -0800

    Connected sewer repairs to API

--- src/app/opscon/maintenance-ops/sewer-repair/new-sewer-repair.component.ts
+++ src/app/opscon/maintenance-ops/sewer-repair/new-sewer-repair.component.ts
@@ -1,36 +1,47 @@
 import {
-  Component
+  OnInit,
+  OnDestroy,
+  Component,
+  ElementRef,
+  ViewChild
 } from '@angular/core';
 import { Http } from '@angular/http';
 import { Router } from '@angular/router';
 
-import {
-  trigger,
-  style,
-  transition,
-  animate
-}  from '@angular/animations';
+import { Subscription } from 'rxjs/Subscription';
 
 import { SelectService, LoggerService } from '../../../shared/services';
-import { SewerCleaning } from './sewer-repair.model';
 import { SewerRepairCommon } from './sewer-repair-common';
 
 @Component({
-  templateUrl: 'new-sewer-repair.component.html',
-  animations: [
-    trigger('routeAnimation', [
-      transition('void => *', [
-        style({ transform: 'translateX(100%)' }),
-        animate('150ms linear')
-      ]),
-      transition('* => void',
-        animate('150ms linear', style({ transform: 'translateX(100%)' }))
-      )
-    ])
-  ]
+  templateUrl: 'new-sewer-repair.component.html'
 })
-export class NewSewerRepairComponent extends SewerRepairCommon {
-  model: SewerCleaning;
+export class NewSewerRepairComponent extends SewerRepairCommon implements OnDestroy, OnInit {
+  @ViewChild('sewerRepairForm') form: ElementRef;
+
+  selectedTotal = 0;
+  selectedFeatures: ol.Collection<ol.Feature>;
+  selector: ol.interaction.Select;
+  countSubscription: Subscription;
+  featuresSubscription: Subscription;
+  savable = false;
+
+  ngOnInit () {
+    // Set custom layer filter
+    this.selectService.setLayerFilter(this.layerFilter);
+
+    // Subscribe to features count
+    this.countSubscription = this.selectService.selectCountObservable.subscribe(count => this.selectedTotal = count);
+    this.featuresSubscription = this.selectService.selectionObservable.subscribe(this.featuresSelected.bind(this));
+
+    // Zoom to extent of current selection
+    this.selectService.zoomToSelection(true);
+  }
+
+  ngOnDestroy () {
+    this.selectService.restoreLayerFilter();
+    this.countSubscription.unsubscribe();
+  }
 
   constructor (
     public selectService: SelectService,
@@ -39,18 +50,41 @@ export class NewSewerRepairComponent extends SewerRepairCommon {
     public router: Router
   ) {
     super(selectService, logger);
-    this.model = new SewerCleaning();
+  }
+
+  featuresSelected (features) {
+    this.savable = false;
+    this.selectedFeatures = features;
+
+    if (features && features.getLength() > 1) {
+      // Do check that one is pipe and one is manhole
+      try {
+        let ids = features.getArray().map(f => f.getId().split('.')[0]);
+        this.savable = ids.length === 2 && ids.includes('gravity_mains') && ids.includes('manholes');
+      } catch (e) {
+        this.savable = false;
+      }
+    }
   }
 
   saveSewerRepair (e) {
+    if (!this.savable) {
+      alert('You must select exactly one pipe and one manhole to save this form.');
+      return;
+    }
+
     e.target.disabled = true;
+
+    let formData = new FormData(this.form.nativeElement);
 
     this.selectedFeatures.forEach(f => {
       let att = f.getProperties();
-      this.model.pipes.push(att.facilityid);
+      let [type, id] = f.getId().split('.');
+      if (type === 'gravity_mains') formData.append('sewer_repair[pipe_id]', att.id);
+      else if (type === 'manholes') formData.append('sewer_repair[manhole_id]', att.id);
     });
 
-    this.http.post('//ch.ci.garden-grove.ca.us/pwams-api/sewer-repairs', this.model).subscribe(
+    this.http.post('//ch.ci.garden-grove.ca.us/pwams-api/sewer-repairs.json', formData).subscribe(
       r => this.router.navigateByUrl('/maintenance-ops/home')
     );
   }
--- src/app/opscon/maintenance-ops/sewer-repair/sewer-repair-common.ts
+++ src/app/opscon/maintenance-ops/sewer-repair/sewer-repair-common.ts
@@ -1,53 +1,14 @@
-import {
-  OnInit,
-  OnDestroy,
-  HostBinding
-} from '@angular/core';
-
-import {
-  trigger,
-  style,
-  transition,
-  animate
-}  from '@angular/animations';
-
 import { SelectService, LoggerService } from '../../../shared/services';
 import { Subscription } from 'rxjs/Subscription';
 
-export class SewerRepairCommon implements OnDestroy, OnInit {
-  @HostBinding('@routeAnimation') routeAnimation = true;
-  @HostBinding('class.maint-ops-route') maintOpsRoute = true;
-
-  selectedTotal = 0;
-  selectedFeatures: ol.Collection<ol.Feature>;
-  selector: ol.interaction.Select;
-  countSubscription: Subscription;
-  featuresSubscription: Subscription;
-
-  ngOnInit () {
-    // Set custom layer filter
-    this.selectService.setLayerFilter(this.layerFilter);
-
-    // Subscribe to features count
-    this.countSubscription = this.selectService.selectCountObservable.subscribe(count => this.selectedTotal = count);
-    this.featuresSubscription = this.selectService.selectionObservable.subscribe(features => this.selectedFeatures = features);
-
-    // Zoom to extent of current selection
-    this.selectService.zoomToSelection(true);
-  }
-
-  ngOnDestroy () {
-    this.selectService.restoreLayerFilter();
-    this.countSubscription.unsubscribe();
-  }
-
+export class SewerRepairCommon {
   constructor (
     public selectService: SelectService,
     public logger: LoggerService
   ) {
   }
 
-  private layerFilter (feature: ol.Feature, layer: ol.layer.Layer): boolean {
+  protected layerFilter (feature: ol.Feature, layer: ol.layer.Layer): boolean {
     let layerName;
 
     if (layer) {
@@ -58,6 +19,6 @@ export class SewerRepairCommon implements OnDestroy, OnInit {
       return false;
     }
 
-    return /ssmanholes$/.test(layerName);
+    return true;
   }
 }
