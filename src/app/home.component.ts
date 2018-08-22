c1357fbce54126714068720113c250e2002d7238
Rachot Moragraan <janechii@gmail.com>
2018-08-21 22:05:52 -0700

    Default dataset can now be set via URL

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  OnInit,
+  Component
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { MapService, LoggerService } from './shared/services/';
 
 @Component({
   selector: 'app-home',
   template: ''
 })
-export class HomeComponent { }
+
+export class HomeComponent implements OnInit {
+  ngOnInit() {
+    let urlDataset;
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      urlDataset = params['dataset'];
+    });
+
+    if (urlDataset) {
+      this.mapService.getMapConfig().then(() => {
+        this.mapService.setCurrentDataset(urlDataset);
+      });
+    }
+  }
+
+  constructor(
+    private logger: LoggerService,
+    private mapService: MapService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+}
--- src/app/opscon/maintenance-ops/home.component.ts
+++ src/app/opscon/maintenance-ops/home.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
-import { MapService, LoggerService }    from '../../shared/services/';
-
 import { MaintenanceOpsService } from './maintenance-ops.service';
 import { Operation } from './operation';
 
@@ -12,24 +8,7 @@ import { Operation } from './operation';
 export class HomeComponent {
   public availableOperations: Array<Operation>;
 
-  constructor (
-    private mos: MaintenanceOpsService,
-    private logger: LoggerService,
-    private mapService: MapService,
-    private activatedRoute: ActivatedRoute
-  ) {
+  constructor(private mos: MaintenanceOpsService) {
     mos.getAvailableOperations().then(ops => this.availableOperations = ops);
-
-    let urlDataset;
-
-    this.activatedRoute.queryParams.subscribe(params => {
-      urlDataset = params['dataset'];
-    });
-
-    if (urlDataset) {
-      this.mapService.getMapConfig().then(() => {
-        this.mapService.setCurrentDataset(urlDataset);
-      });
-    }
   }
 }
